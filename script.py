92947d76358e27c418b91716db23f616e7ac39a5
Ivan G Stewart <71293769+ivanstewart2001@users.noreply.github.com>
2024-03-25 10:01:37 -0400

    first commit

---
+++ script.py
@@ -0,0 +1,56 @@
+import os
+import pandas as pd
+import numpy as np
+from nltk.corpus import stopwords
+import nltk
+nltk.download('stopwords')
+import re
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.metrics.pairwise import cosine_similarity
+from sklearn.metrics.pairwise import euclidean_distances
+
+current_directory = os.getcwd()
+
+def getPath(filename):
+    csv_path = os.path.join(current_directory, filename)
+    return csv_path
+
+numberOfRows = 10
+oneStarReviews = pd.read_csv(getPath('one_star_reviews_sample.csv'),  nrows=numberOfRows)
+
+documents = []
+
+for index, row in oneStarReviews.iterrows():
+    review_text = row['Reviews']
+    documents.append(review_text)
+
+documents_df=pd.DataFrame(documents,columns=['documents'])
+stop_words=stopwords.words('english')
+documents_df['documents_cleaned']=documents_df.documents.apply(lambda x: " ".join(re.sub(r'[^a-zA-Z]',' ',w).lower() for w in x.split() if re.sub(r'[^a-zA-Z]',' ',w).lower() not in stop_words) )
+
+tfidfvectoriser=TfidfVectorizer()
+tfidfvectoriser.fit(documents_df.documents_cleaned)
+tfidf_vectors=tfidfvectoriser.transform(documents_df.documents_cleaned)
+
+pairwise_similarities=np.dot(tfidf_vectors,tfidf_vectors.T).toarray()
+pairwise_differences=euclidean_distances(tfidf_vectors)
+
+def most_similar(doc_id,similarity_matrix,matrix):
+    print (f'Document: {documents_df.iloc[doc_id]["documents"]}')
+    print ('\n')
+    print ('Similar Documents:')
+    if matrix=='Cosine Similarity':
+        similar_ix=np.argsort(similarity_matrix[doc_id])[::-1]
+    elif matrix=='Euclidean Distance':
+        similar_ix=np.argsort(similarity_matrix[doc_id])
+    for ix in similar_ix:
+        if ix==doc_id:
+            continue
+        print('\n')
+        print (f'Document: {documents_df.iloc[ix]["documents"]}')
+        print (f'{matrix} : {similarity_matrix[doc_id][ix]}')
+
+most_similar(0,pairwise_similarities,'Cosine Similarity')
+most_similar(0,pairwise_differences,'Euclidean Distance')
+
+
